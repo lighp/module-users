628a46dbd02374ebe3850f29e722b2314b004c86
emersion <contact@emersion.fr>
2015-08-25 21:34:58 +0200

    Adds e-mail address to User

--- src/lib/entities/User.class.php
+++ src/lib/entities/User.class.php
@@ -2,16 +2,17 @@
 
 namespace lib\entities;
 
+use InvalidArgumentException;
 use core\Entity;
 
 class User extends Entity {
-	protected $username, $password;
+	protected $username, $password, $email;
 
 	// SETTERS //
 
 	public function setUsername($username) {
 		if (!is_string($username) || empty($username)) {
-			throw new InvalidArgumentException('Invalid user username: empty username');
+			throw new InvalidArgumentException('Invalid user username');
 		}
 		if ($username === 'me') {
 			throw new InvalidArgumentException('Invalid user username: this username is reserved');
@@ -21,9 +22,19 @@ class User extends Entity {
 
 	public function setPassword($password) {
 		if (!is_string($password) || empty($password)) {
-			throw new InvalidArgumentException('Invalid user password: empty password');
+			throw new InvalidArgumentException('Invalid user password');
 		}
 		$this->password = $password;
+	}
+
+	public function setEmail($email) {
+		if (!is_string($email) && $email !== null) {
+			throw new InvalidArgumentException('Invalid user email: not a string');
+		}
+		if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+			throw new InvalidArgumentException('Invalid user email: not a valid email address');
+		}
+		$this->email = $email;
 	}
 
 	// GETTERS //
@@ -34,5 +45,17 @@ class User extends Entity {
 
 	public function password() {
 		return $this->password;
+	}
+
+	public function email() {
+		return $this->email;
+	}
+
+
+	public function toArray() {
+		// For security reasons, do not include password
+		$data = parent::toArray();
+		unset($data['password']);
+		return $data;
 	}
 }
